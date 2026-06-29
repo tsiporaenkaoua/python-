22301d75f469378fe3c64546f71df88065b93a8a
Enkaoua Tsipora <tsipoenkaoua@gmail.com>
2026-06-29 13:50:19 +0200

    python types

---
+++ jour1.py
@@ -0,0 +1,17 @@
+nom = "Tsipora"
+
+age = 25
+
+taille = 1.70
+
+etudiante = True
+
+print(nom)
+print(age)
+print(taille)
+print(etudiante)
+
+print(type(nom))
+print(type(age))
+print(type(taille))
+print(type(etudiante))
